3c7145307512c439105f7baf5ad69fc12c5ef355
Adrian <adrian.stypinski@gmail.com>
2021-04-20 14:16:11 +0200

    Initial commit

---
+++ src/test/kotlin/pl/allegro/stypinski/recruitmenttask/RecruitmentTaskApplicationTests.kt
@@ -0,0 +1,13 @@
+package pl.allegro.stypinski.recruitmenttask
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RecruitmentTaskApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
